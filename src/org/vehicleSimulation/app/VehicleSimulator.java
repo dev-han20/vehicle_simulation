ed6ee3fa1d0ed776cb837921faf32af081f88b20
CodingNoJam <hanwoodww@gmail.com>
2021-05-08 17:30:50 +0900

    ~ing

--- src/org/vehicleSimulation/app/VehicleSimulator.java
+++ src/org/vehicleSimulation/app/VehicleSimulator.java
@@ -1,10 +1,26 @@
 package org.vehicleSimulation.app;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class VehicleSimulator {
 
+	// 이동체정보 저장 할 리스트
+	static List<Vehicle> list = new ArrayList<Vehicle>();  
+	
 	public static void main(String[] args) {
 		System.out.println("###");
+		
+		// 이동체 데이터 받기
+		inputData();
+		
+	}
 
+	
+	private static void inputData() {
+		
+		
+		
 	}
 	
 	
